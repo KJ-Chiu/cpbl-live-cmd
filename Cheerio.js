29d37009a6e0baa999302c340853a31e1339cee9
kjchiu <kj.chiu73@gmail.com>
2018-09-13 22:25:46 +0800

    Create Cheerio with basic filterSchedule function

---
+++ Cheerio.js
@@ -0,0 +1,26 @@
+const cheerio = require('cheerio');
+
+class Cheerio {
+  setData (data) {
+    this.data = data;
+    this.filterData = [];
+  }
+
+  filterSchedule () {
+    const $ = cheerio.load(this.data);
+    let schedule = $('.schedule').first('tbody');
+    $(schedule).children().children('tr').children('th').each((i, elem) => {
+      // 日期欄
+      if ($(elem).hasClass('past') || $(elem).hasClass('today') || $(elem).hasClass('future')) {
+        this.filterScheduleDate($(elem));
+      }
+    });
+  }
+
+  filterScheduleDate (dateColumn) {
+    const $ = cheerio.load(this.data);
+    console.log($(dateColumn).html());
+  }
+}
+
+module.exports = Cheerio;
