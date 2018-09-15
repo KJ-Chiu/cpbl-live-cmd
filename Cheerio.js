1bdc183322b3f817695a08180cc5fbf3a2243e43
kjchiu <kj.chiu73@gmail.com>
2018-09-15 11:54:49 +0800

    Get game info from schedule

--- Cheerio.js
+++ Cheerio.js
@@ -17,6 +17,11 @@ class Cheerio {
         this.filterScheduleDate($(elem));
       }
     });
+    // 賽程欄
+    $(schedule).children().children('tr').children('td').each((i, elem) => {
+      let data = this.filterData[i];
+      this.filterData[i] = this.filterScheduleGame($(elem), data);
+    });
   }
 
   filterScheduleDate (dateColumn) {
@@ -37,6 +42,59 @@ class Cheerio {
     this.filterData.push({
       date: month + '/' + date
     });
+  }
+
+  filterScheduleGame (dataColumn, gameInfo) {
+    const $ = cheerio.load(this.data);
+    gameInfo.games = [];
+    $(dataColumn).children('div').each((i, elem) => {
+      let perGame = {};
+
+      // 對戰組合
+      $(elem).find('.schedule_team').children('tbody').children('tr').children('td').each((j, subElem) => {
+        if (0 == j) {
+          perGame.away = this.teamCodeToName($(subElem).children('img').attr('src'));
+        }
+
+        if (1 == j) {
+          perGame.place = $(subElem).text();
+        }
+
+        if (2 == j) {
+          perGame.home = this.teamCodeToName($(subElem).children('img').attr('src'));
+        }
+      });
+
+      // 比賽編號
+      $(elem).find('.schedule_info').first().children('tbody').children('tr').children('th').each((j, subElem) => {
+        if (1 == j) {
+          perGame.code = $(subElem).text();
+        }
+      });
+      gameInfo.games.push(perGame);
+    });
+    return gameInfo;
+  }
+
+  // 由於賽程表上面沒有名稱，故只能利用圖檔的 src 來做分類
+  teamCodeToName (imgSrc) {
+    if (imgSrc.includes('B')) {
+      return '富邦';
+    }
+
+    if (imgSrc.includes('L')) {
+      return '統一';
+    }
+
+    if (imgSrc.includes('E')) {
+      return '中信';
+    }
+
+    if (imgSrc.includes('A')) {
+      return '桃猿';
+    }
+
+    return '未知';
   }
 }
 
