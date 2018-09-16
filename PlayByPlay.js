f66963681a15c181c64632c3d920f84b24ca0e87
kjchiu <kj.chiu73@gmail.com>
2018-09-16 21:23:13 +0800

    Create PlayByPlay.js

---
+++ PlayByPlay.js
@@ -0,0 +1,24 @@
+const Request = require('./Request');
+const Cheerio = require('./Cheerio');
+
+class PlayByPlay {
+  init (gameId, gameDate, pbyear, data) {
+    this.gameId = gameId;
+    this.gameDate = gameDate;
+    this.pbyear = pbyear;
+    this.initData = data;
+    this.refresh();
+  }
+
+  refresh () {
+    return setTimeout(() => {
+      return this.getData();
+    }, 2000);
+  }
+
+  getData () {
+    return this.refresh();
+  }
+}
+
+module.exports = PlayByPlay;
