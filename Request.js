6e3330f9cd4ec31975243c5efeb7c43502935c53
kjchiu <kj.chiu73@gmail.com>
2018-09-16 18:01:03 +0800

    Create gamePlayByPlay function

--- Request.js
+++ Request.js
@@ -4,6 +4,8 @@ class Request {
   constructor () {
     this.scheduleUrl = 'http://www.cpbl.com.tw/schedule/index/';
     this.scheduleUrlAppend = '.html?gameno=01&sfieldsub=&sgameno=01&date=';
+
+    this.playByPlayUrl = 'http://www.cpbl.com.tw/games/play_by_play.html?&game_type=01';
   }
 
   schedule (targetDate, callback) {
@@ -17,6 +19,19 @@ class Request {
         return callback(false);
       }
       console.log('Successfully get game schedule from cpbl');
+      return callback(b);
+    });
+  }
+
+  gamePlayByPlay (gameId, gameDate, pbyear, callback) {
+    request({
+      url:  `${this.playByPlayUrl}&game_id=${gameId}&game_date=${gameDate}&pbyear=${pbyear}`,
+      method: 'GET'
+    }, function (e, r, b) {
+      if (e || !b) {
+        console.error('[ERROR] Can\'t get data from cpbl play by play, stop.');
+        return callback(false);
+      }
       return callback(b);
     });
   }
